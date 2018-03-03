e57ab9e98a1226361c3a756b9ade425c7396cb19
Tusharj09 <tusharjaiswal975@gmail.com>
2018-03-03 11:50:44 +0530

    custom2 and 3 dot image button in actorbarlayout
    custom2 and 3 dot image button in actorbarlayout

--- app/src/main/java/com/rent/rentmanagement/renttest/StudentActivity.java
+++ app/src/main/java/com/rent/rentmanagement/renttest/StudentActivity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import org.json.JSONException;
